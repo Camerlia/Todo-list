23f8ea83c0dbba9aab748d12d78c019c8089a5c3
Camerlia <cameliaafolabi@gmail.com>
2024-12-15 12:28:14 +0100

    completed the styling for the ui

--- src/components/todo-list/Todo.jsx
+++ src/components/todo-list/Todo.jsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
-
+import { MdDeleteForever } from "react-icons/md";
+import { FaRegCheckCircle } from "react-icons/fa";
 const Todo = () => {
   const initialVal = {
     stateVal: false,
@@ -28,29 +29,31 @@ const Todo = () => {
   };
 
   return (
-    <div className="max-w-5xl m-auto bg-orange-900/80 p-10 rounded-md">
-      <h1 className="font-extrabold text-4xl py-3">To-Do List</h1>
-      <div className="flex flex-row gap-4 items-center justify-center py-4">
+    <div className="max-w-5xl mx-auto bg-orange-900/80 p-4 rounded-md">
+      <h1 className="text-3xl md:text-4xl font-extrabold py-3 text-center">
+        To-Do List
+      </h1>
+      <div className="flex flex-col md:flex-row gap-4 md:items-center md:justify-center py-4">
         <div className="flex flex-col">
-          <label>Name</label>
+          <label className="text-sm md:text-base pb-2">Name</label>
           <input
             type="text"
-            placeholder="input name"
-            className="rounded-sm border-2"
+            placeholder="Input name"
+            className="rounded-sm border-2 w-full md:w-64"
           />
         </div>
         <div className="flex flex-col">
-          <label>Description</label>
+          <label className="text-sm md:text-base pb-2">Description</label>
           <input
             type="text"
-            placeholder="input description"
-            className="rounded-sm border-2"
+            placeholder="Input description"
+            className="rounded-sm border-2 w-full md:w-64"
           />
         </div>
-        <div>
+        <div className="flex justify-start items-start">
           <button
             type="button"
-            className="p-4 rounded-sm border bg-orange-700 hover:bg-orange-500 border-white"
+            className="md:p-4 p-1 w-full md:max-w-lg m-auto rounded-sm border bg-orange-700 hover:bg-orange-500 border-white"
           >
             Add
           </button>
@@ -74,9 +77,15 @@ const Todo = () => {
           Completed
         </button>
       </div>
-      <div className="flex flex-col gap-4 py-4">
-        <h3>Task</h3>
-        <p>Description</p>
+      <div className=" bg-orange-500/90 p-4 flex flex-col md:flex-row md:justify-between md:items-center mt-5 gap-6" >
+        <div className="flex flex-col gap-4 ">
+          <h3 className="text-lg font-semibold md:text-3xl">Task</h3>
+          <p className="text-base font-extralight">Description</p>
+        </div>
+        <div className="flex gap-2 md:gap-4 ">
+          <MdDeleteForever className="text-lg md:text-3xl hover:text-red-800" />
+          <FaRegCheckCircle className="text-lg md:text-3xl hover:text-orange-200" />
+        </div>
       </div>
     </div>
   );
