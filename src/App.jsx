ab6354c16cf4c5651cd53375286e0a00cf56a627
Camerlia <cameliaafolabi@gmail.com>
2024-12-17 11:44:16 +0100

    fixed potential errors in the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { TodoProvider } from "./components/context/toggleProvider";
-import Todo from "./components/todo-list/todo";
+import Todo from "./components/todo-list/Todo";
+
 
 function App() {
   return (
     <div className="bg-orange-950 h-screen text-white py-14 flex flex-col justify-center">
       <TodoProvider>
         <Todo />
+        
       </TodoProvider>
     </div>
   );
